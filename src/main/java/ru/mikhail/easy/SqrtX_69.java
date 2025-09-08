80c10be1ba57d8f64469d1bc02b8233c20f26196
Mikhail Simonishvili <mikhail.simonishvili@dukascopy.com>
2025-09-08 10:16:48 +0400

    task: 69 https://leetcode.com/problems/sqrtx/

---
+++ src/main/java/ru/mikhail/easy/SqrtX_69.java
@@ -0,0 +1,31 @@
+package ru.mikhail.easy;
+
+public class SqrtX_69 {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(2147395599));
+    }
+
+
+    private static int mySqrt(int x) {
+        if (x <= 1) return x;
+        long closest, current;
+        closest = 1;
+        long r = x;
+        long l = 0L;
+        while (closest > 0) {
+            current = (l + r) / 2;
+            long sqr = current * current;
+            if (sqr == x) {
+                return (int) current;
+            } else if (sqr < x) {
+                closest = Math.max(current, closest);
+                if (r - l == 1 || r == l) return (int) closest;
+                l = current;
+            } else {
+                if (r - l == 1 || r == l) return (int) closest;
+                r = current;
+            }
+        }
+        return (int) closest;
+    }
+}
