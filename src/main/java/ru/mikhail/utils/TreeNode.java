2005d78901afbbb77048d6ccd1e25bcacd5f5b28
SimonishviliMA <simonishvili.ma@gmail.com>
2026-02-22 10:59:28 +0400

    To TreeNode was added static utility method which converts Integer[] to TreeNode

--- src/main/java/ru/mikhail/utils/TreeNode.java
+++ src/main/java/ru/mikhail/utils/TreeNode.java
@@ -1,6 +1,9 @@
 package ru.mikhail.utils;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -11,6 +14,34 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode arrayToTreeNode(Integer[] vals) {
+        if (vals == null || vals.length == 0 || vals[0] == null) return null;
+
+        TreeNode root = new TreeNode(vals[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int i = 1;
+        while(!q.isEmpty() && i < vals.length) {
+            TreeNode curr = q.poll();
+
+            Integer leftVal = vals[i++];
+            if (leftVal != null) {
+                curr.left = new TreeNode(leftVal);
+                q.add(curr.left);
+            }
+
+            if (i < vals.length) {
+                Integer rightVal = vals[i++];
+                if (rightVal != null) {
+                    curr.right = new TreeNode(rightVal);
+                    q.add(curr.right);
+                }
+            }
+        }
+        return root;
     }
 
     @Override
