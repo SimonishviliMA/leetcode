2dc656ac4aeae4b794c07176233656904c5ae771
SimonishviliMA <simonishvili.ma@gmail.com>
2026-06-30 15:45:50 +0400

    task 142: https://leetcode.com/problems/linked-list-cycle-ii/

---
+++ src/main/java/ru/mikhail/medium/LinkedListCycleII_142.java
@@ -0,0 +1,24 @@
+package ru.mikhail.medium;
+
+import ru.mikhail.utils.ListNode;
+
+public class LinkedListCycleII_142 {
+
+    private static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (head != fast) {
+                    head = head.next;
+                    fast = fast.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+}
